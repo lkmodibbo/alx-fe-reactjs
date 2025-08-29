df567aed38ed006210921e4c1c852d25790bcf3a
lkmodibbo <lukmansaidmodibbo@gmail.com>
2025-08-29 15:33:38 +0100

    corrected user profile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,7 +3,7 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import WelcomeMessage from './components/WelcomeMessage'
-import { UserProfile } from './components/UserProfile'
+import UserProfile from './components/UserProfile'
 
 function App() {
 
