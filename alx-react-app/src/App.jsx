a7c2f18a34ccd82d0e1effec2314381873dd68de
lkmodibbo <lukmansaidmodibbo@gmail.com>
2025-08-29 15:12:25 +0100

    header corrected

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
-import  Footer  from './Footer'
-import  Header  from './Header'
-import  MainContent  from '.MainContent'
+import  Footer  from './components/Footer'
+import  Header  from './components/Header'
+import  MainContent  from './components/MainContent'
 import WelcomeMessage from './components/WelcomeMessage'
 
 function App() {
