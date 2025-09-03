920cd9a9194a8707619d2cbd88069557a78737c5
lkmodibbo <lukmansaidmodibbo@gmail.com>
2025-09-03 12:15:31 +0100

    applied inline styling in the styles

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
+import WelcomeMessage from './components/WelcomeMessage'
+import UserProfile from './components/UserProfile'
+
+function App() {
+
+
+  return (
+    <div>
+      <Header />
+      <WelcomeMessage />
+      <MainContent />
+      <UserProfile 
+        name='Alice'
+        age={25}
+        bio="Loves hiking and photography"
+      />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
