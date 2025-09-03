c0c16868b25fdfdf760bdce318e059233c9ed7cb
lkmodibbo <lukmansaidmodibbo@gmail.com>
2025-09-03 12:37:23 +0100

    counter.jsx added

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,23 +1,20 @@
 import './App.css'
 import Header from './components/Header'
-import MainContent from './components/MainContent'
 import Footer from './components/Footer'
-import WelcomeMessage from './components/WelcomeMessage'
 import UserProfile from './components/UserProfile'
+import Counter from './components/Counter'
 
 function App() {
 
 
   return (
     <div>
-      <Header />
-      <WelcomeMessage />
-      <MainContent />
       <UserProfile 
         name='Alice'
         age={25}
         bio="Loves hiking and photography"
       />
+      <Counter />
       <Footer />
     </div>
   )
