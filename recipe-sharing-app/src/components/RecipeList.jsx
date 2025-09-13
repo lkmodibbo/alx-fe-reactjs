781bf92c1f06fe0c7312b4f614a74d16cb038319
lkmodibbo <lukmansaidmodibbo@gmail.com>
2025-09-13 14:22:06 +0100

    link added to the recipe list

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,20 +1,28 @@
 import { useRecipeStore } from './recipeStore'
+import { Link } from 'react-router-dom'
 
 const RecipeList = () => {
   const recipes = useRecipeStore(state => state.recipes)
   const filteredRecipes = useRecipeStore(state => state.filteredRecipes)
   const searchTerm = useRecipeStore(state => state.searchTerm)
 
-  // If there's a search term, use filteredRecipes; otherwise, show all recipes
   const recipesToDisplay = searchTerm ? filteredRecipes : recipes
 
   return (
     <div>
       {recipesToDisplay.length > 0 ? (
         recipesToDisplay.map(recipe => (
-          <div key={recipe.id}>
-            <h3>{recipe.title}</h3>
+          <div key={recipe.id} className="border p-4 mb-3 rounded bg-gray-100">
+            <h3 className="font-bold text-lg">{recipe.title}</h3>
             <p>{recipe.description}</p>
+            
+            {/* 👇 Link to recipe details page */}
+            <Link 
+              to={`/recipes/${recipe.id}`} 
+              className="text-blue-500 underline"
+            >
+              View Details
+            </Link>
           </div>
         ))
       ) : (
