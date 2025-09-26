38a6cb881a9d2f208b6c53dde18b299f96b85a55
lkmodibbo <lukmansaidmodibbo@gmail.com>
2025-09-26 17:54:49 +0100

    sm:h-24

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@ function UserProfile() {
   return (
     <div className="user-profile bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
         <img 
-            className="mx-auto rounded-full w-24 h-24 sm:w-24 md:h-36 sm:h-36 md:w-36"
+            className="mx-auto rounded-full w-24 h-24 sm:w-24 md:h-36 sm:h-36 md:w-36 sm:h-24"
             src="/lk.jpg" 
             alt="User" 
         />
