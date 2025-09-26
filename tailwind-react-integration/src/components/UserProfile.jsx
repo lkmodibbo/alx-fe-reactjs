f8399ea5db081ac2bce181c9e7603d99253abf24
lkmodibbo <lukmansaidmodibbo@gmail.com>
2025-09-26 16:45:44 +0100

    h1 corrected

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -5,7 +5,7 @@ function UserProfile() {
       <img 
         className="mx-auto rounded-full w-36 h-36"
         src="https://via.placeholder.com/150" alt="User" />
-      <h1 className="text-xl text-blue-900 my-4">John Doe</h1>
+      <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
       <p className="text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );
