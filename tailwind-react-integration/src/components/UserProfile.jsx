45e45ca31b71746a60782edda145631a4014c254
lkmodibbo <lukmansaidmodibbo@gmail.com>
2025-09-26 17:30:37 +0100

    responsiveness added

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,15 @@
-
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div className="user-profile bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
       <img 
-        className="mx-auto rounded-full w-36 h-36"
-        src="https://via.placeholder.com/150" alt="User" />
-      <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-      <p className="text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
+        className="mx-auto rounded-full w-36 h-36 sm: w-24 h-24 md: w-36 h-36"
+        src="/lk.jpg" 
+        alt="User" 
+      />
+      <h1 className="text-xl text-blue-900 my-4 sm: text-lg md: text-xl">John Doe</h1>
+      <p className="text-gray-600 text-base sm: text-sm md: text-base">
+        Developer at Example Co. Loves to write code and explore new technologies.
+      </p>
     </div>
   );
 }
