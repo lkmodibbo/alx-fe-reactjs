95eb1a6432325ccd072ed7d0f018b67e8cc5b247
lkmodibbo <lukmansaidmodibbo@gmail.com>
2025-10-01 22:08:00 +0100

    recipe npm set up connected

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import './index.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">Hello Tailwind!</h1>
+      <button className="mt-4 px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600">
+        Test Button
+      </button>
+    </div>
+  )
+}
+
+export default App
+
+
